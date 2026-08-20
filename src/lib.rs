152c2d6306285f93325218fb1260cbc3d1680950
AlexMa178 <alexcma27@gmail.com>
2026-08-20 14:06:29 -0700

    fixed put_multi

--- src/lib.rs
+++ src/lib.rs
@@ -57,14 +57,14 @@ impl<T: Tile, const W: usize, const H: usize> TileGrid<T, W, H> {
         let w = NumCast::from(W).unwrap();
         let h = NumCast::from(H).unwrap();
         let mut canvas = PixelCanvas::new::<T::TileUnit>(gfx, [ w, h ]);
-        let xs = { let mut dx = ConstZero::ZERO; iter::from_fn(move || { dx += ConstOne::ONE; if dx < w { Some(dx) } else { None } }) };
-        let ys = { let mut dy = ConstZero::ZERO; iter::from_fn(move || { dy += ConstOne::ONE; if dy < h { Some(dy) } else { None } }) };
-        for dx in xs {
-            for dy in ys.clone() {
-                let [ atl_x, atl_y ] = self.at(dx, dy).atlas_pos();
+        let xs = { let mut x = ConstZero::ZERO; iter::from_fn(move || { x += ConstOne::ONE; if x < w { Some(x) } else { None } }) };
+        let ys = { let mut y = ConstZero::ZERO; iter::from_fn(move || { y += ConstOne::ONE; if y < h { Some(y) } else { None } }) };
+        for x in xs {
+            for y in ys.clone() {
+                let [ atlas_x, atlas_y ] = self.at(x, y).atlas_pos();
                 canvas.draw(atlas, PixelDrawParams::<T::PixelUnit>::default()
-                    .dest::<T::TileUnit>([ dx, dy ])
-                    .atlas_rect::<T::TileUnit>(([ atl_x, atl_y ], [ ConstOne::ONE, ConstOne::ONE ]))
+                    .dest::<T::TileUnit>([ x, y ])
+                    .atlas_rect::<T::TileUnit>(([ atlas_x, atlas_y ], [ ConstOne::ONE, ConstOne::ONE ]))
                 );
             }
         }
@@ -114,7 +114,7 @@ impl<T: Tile, const W: usize, const H: usize> TileGridBuilder<T, W, H> {
         let ys = { let mut dy = ConstZero::ZERO; iter::from_fn(move || { dy += ConstOne::ONE; if dy < h { Some(dy) } else { None } }) };
         for dx in xs {
             for dy in ys.clone() {
-                *self.tg.at_mut(dx, dy) = v.sub(dx, dy);
+                *self.tg.at_mut(self.x + dx, self.y + dy) = v.sub(dx, dy);
             }
         }
         self.move_x(w)
